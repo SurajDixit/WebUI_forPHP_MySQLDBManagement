4179d2d5ec7ddb0c4402332c03b99e94418db03f
Suraj U Dixit <suraj.bvbcet@outlook.com>
2018-02-15 17:56:05 -0800

    code changes query.php

--- query.php
+++ query.php
@@ -19,51 +19,59 @@ if(strtoupper($checkType[0]) == "SELECT")
         }
     }
     
-    echo "<table border=1 align='center'>";
     
-    //getting table headers
-    $sql1 = "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='".$db."' AND `TABLE_NAME`='".$tablename."'";
-    $result1 = mysqli_query($conn, $sql1);
-    if(mysqli_num_rows($result1) > 0)
-    {
-        echo "<tr>";
-        while($row1 = mysqli_fetch_array($result1,MYSQLI_NUM))
-        {
-            $num2 = sizeof($row1);
-            for($i=0;$i<$num2;$i++)
-            {
-                echo "<th>".$row1[$i]."</th>";
-            }
-        }
-        echo "</tr>";
-    }
-    
-    //getting the contents
     if(mysqli_num_rows($result) > 0)
+        $resultCount = 1;
+    else
+        $resultCount = 0;
+  
+  //if count of tuples more than 1
+    if($resultCount == 1)  
     {
-        while($row = mysqli_fetch_array($result,MYSQLI_NUM))
+        echo "<table border=1 align='center'>";
+        
+        //getting table headers
+        $sql1 = "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='".$db."' AND `TABLE_NAME`='".$tablename."'";
+        $result1 = mysqli_query($conn, $sql1);
+        if(mysqli_num_rows($result1) > 0)
         {
             echo "<tr>";
-            $num = sizeof($row);
-            for($i=0;$i<$num;$i++)
+            while($row1 = mysqli_fetch_array($result1,MYSQLI_NUM))
             {
-                echo "<td>".$row[$i]."</td>";
+                $num2 = sizeof($row1);
+                for($i=0;$i<$num2;$i++)
+                {
+                    echo "<th>".$row1[$i]."</th>";
+                }
             }
             echo "</tr>";
-            $count = $count + 1;
         }
         
-        //success
-        echo "</table>";
-        echo "<br><center>".$count." rows returned for '".$sql."'!";
-        echo "<br><a href='https://jarvis-test.azurewebsites.net/query_form.html'>Go back to Query-form page</a></center>";
+        //getting the contents
+    
+            while($row = mysqli_fetch_array($result,MYSQLI_NUM))
+            {
+                echo "<tr>";
+                $num = sizeof($row);
+                for($i=0;$i<$num;$i++)
+                {
+                    echo "<td>".$row[$i]."</td>";
+                }
+                echo "</tr>";
+                $count = $count + 1;
+            }
+            
+            //success
+            echo "</table>";
+            echo "<br><center>".$count." rows returned for '".$sql."'!";
+            echo "<br><a href='query_form.html'>Go back to Query-form page</a></center>";
     } 
     else 
     {
         //failure
     	echo "<center>0 rows returned for '".$sql."'!<br>";
     	echo mysqli_error($conn)."<br>";
-    	echo "<a href='https://jarvis-test.azurewebsites.net/query_form.html'>Go back to Query-form page</a></center>";
+    	echo "<a href='query_form.html'>Go back to Query-form page</a></center>";
     }
 }
 
@@ -74,7 +82,7 @@ else
     {
         //success
         echo "<center>Query '".$sql."' executed successfully<br>";
-        echo "<a href='https://jarvis-test.azurewebsites.net/query_form.html'>Go back to Query-form page</a></center>";
+        echo "<a href='query_form.html'>Go back to Query-form page</a></center>";
     } 
     else 
     {
